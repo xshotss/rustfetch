ae3faca6ee5c3bfecb320df3bfe5306c255548e7
xshotss <xshotsba@gmail.com>
2025-09-07 23:31:58 +0300

    Made default ASCII art smaller

--- src/lib.rs
+++ src/lib.rs
@@ -2,47 +2,46 @@ pub mod info;
 pub mod modules;
 
 pub const DEFAULT_LUA_CONFIG: &str = r#"
--- This is an automatically generating config file for Rustfetch.
+-- This is an automatically generated config file for Rustfetch.
 -- Check the Github repo for help:
 -- https://github.com/xshotss/rustfetch
 
 -- All ASCII art files should be placed in ~/.config/rustfetch/ascii/
-ascii_art = "tux.txt"
+ascii = "tux.txt"
 
--- For configuring your own modules, go to the repository for help.
-default_modules = true
+mode = "fancy"
 
-module_mode = "fancy"
+modules = {
+  cpu = {
+    type = "builtin"
+  },
+  gpu = {
+    type = "builtin"
+  },
+  hostname = {
+    type = "builtin"
+  }
+}
 "#;
 
 /// A built in const containing ASCII art of the Linux mascot, Tux.
 pub const TUX_ASCII_ART: &str = r#"
-                .88888888:.
-               88888888.88888.
-             .8888888888888888.
-             888888888888888888
-             88' _`88'_  `88888
-             88 88 88 88  88888
-             88_88_::_88_:88888
-             88:::,::,:::::8888
-             88`:::::::::'`8888
-            .88  `::::'    8:88.
-           8888            `8:888.
-         .8888'             `888888.
-        .8888:..  .::.  ...:'8888888:.
-       .8888.'     :'     `'::`88:88888
-      .8888        '         `.888:8888.
-     888:8         .           888:88888
-   .888:88        .:           888:88888:
-   8888888.       ::           88:888888
-   `.::.888.      ::          .88888888
-  .::::::.888.    ::         :::`8888'.:.
- ::::::::::.888   '         .::::::::::::
- ::::::::::::.8    '      .:8::::::::::::.
-.::::::::::::::.        .:888:::::::::::::
-:::::::::::::::88:.__..:88888:::::::::::'
- `'.:::::::::::88888888888.88:::::::::'
-       `':::_:' -- '' -'-' `':_::::'`
+         _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|
+       @,----.JM|
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     `-'       `--'
 "#;
 
 #[cfg(test)]
